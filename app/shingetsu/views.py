451131a8554eb86b08446a1943133471cee4bb4f
yuuki <yuuki0xff@gmail.com>
2015-03-14 02:04:52 +0900

    shingetsu: pingを実装

--- app/shingetsu/views.py
+++ app/shingetsu/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
-from models import *
+from shingetsu.models import *
 
 class ping(View):
-	pass
+	def dispatch(self, request, *args, **kwargs):
+		addr = request.META['REMOTE_ADDR']
+		response = HttpResponse()
+		response.write("PONG\n"+str(addr))
+		s = Session()
+		if not s.query(Node).filter(Node.host == addr).first():
+			s.add(Node(host=addr))
+			s.commit()
+		s.close()
+		return response
 
 class node(View):
 	pass

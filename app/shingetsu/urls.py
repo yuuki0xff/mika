2c2313e1c79752612bb5fa13784af3c50c501a06
yuuki <yuuki0xff@gmail.com>
2015-03-14 02:00:45 +0900

    shingetsu: urlディスパッチャの設定をクラスベースの指定方式に修正
    関数ベースの古い方式は、新しいVarだと廃止されていた。

--- app/shingetsu/urls.py
+++ app/shingetsu/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import patterns, include, url
+from app.shingetsu.views import *
 
 _node = '(?P<node>.+)'
 _file = '(?P<file>[^/]+)'
 _time = '(?P<time>.*)'
 
-urlpatterns = patterns('shingetsu.views',
-	url(r'^ping$', 'ping'),
-	url(r'^node$', 'node'),
-	url(r'^join/'+_node+'$', 'join'),
-	url(r'^bye/'+_node+'$', 'bye'),
-	url(r'^have/'+_file+'$', 'have'),
-	url(r'^get/'+_file+'/'+_time+'$', 'get'),
-	url(r'^head/'+_file+'/'+_time+'$', 'head'),
-	url(r'^update/'+_file+'/'+_time+'$', 'update'),
+urlpatterns = patterns('',
+	url(r'^ping$', ping.as_view()),
+	url(r'^node$', node.as_view()),
+	url(r'^join/'+_node+'$', join.as_view()),
+	url(r'^bye/'+_node+'$',  bye.as_view()),
+	url(r'^have/'+_file+'$', have.as_view()),
+	url(r'^get/'+_file+'/'+_time+'$', get.as_view()),
+	url(r'^head/'+_file+'/'+_time+'$', head.as_view()),
+	url(r'^update/'+_file+'/'+_time+'$', update.as_view()),
 	url(r'^recent/'+_time+'$', 'recent'),
 )
 

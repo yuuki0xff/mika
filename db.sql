3844dd543410bb3b9b651b6cfa84688650dea485
yuuki <yuuki0xff@gmail.com>
2015-03-14 18:18:01 +0900

    DBの再設計
    行ったこと
     * threadが保持するレコード数の更新を行うトリガの不都合を修正
     * threadのtimestampのデフォルト値を設定
     * テーブルのリネーム
        record_remove -> record_remove_notify
     * 新しいテーブルの追加
       record_removed
     * インデックスの見直し
     * カラム数の削減

--- db.sql
+++ db.sql
@@ -1,67 +1,72 @@
 
--- version: 0.0.1
+-- version: 0.0.2
 DROP DATABASE IF EXISTS mika;
 CREATE DATABASE mika;
 USE mika;
 
 CREATE TABLE SYSTEM(
 	id CHAR(10),
-	value CHAR(10),
+	value VARCHAR(255),
 	PRIMARY KEY(id)
 );
+INSERT INTO SYSTEM(id, value) VALUES('version', '0.0.2');
 
 CREATE TABLE thread(
 	-- titleのサイズは適当
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	title CHAR(255) NOT NULL,
-	timestamp TIMESTAMP NOT NULL,
+	timestamp TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
 	records INT NOT NULL DEFAULT 0,
-	UNIQUE i_title(title)
+	removed_records INT NOT NULL DEFAULT 0,
+	UNIQUE(title),
+	INDEX(timestamp, id)
 );
 
 CREATE TABLE record(
-	thread_id INT UNSIGNED,
-	id INT UNSIGNED AUTO_INCREMENT,
-	bin_id BINARY(16),
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	thread_id INT UNSIGNED NOT NULL,
+	bin_id BINARY(16) NOT NULL,
 	timestamp TIMESTAMP NOT NULL,
+	is_post BOOLEAN NOT NULL DEFAULT TRUE,
 	is_remove_notify BOOLEAN NOT NULL DEFAULT FALSE,
 	is_attach BOOLEAN NOT NULL DEFAULT FALSE,
-	PRIMARY KEY(id),
-	INDEX(thread_id, bin_id, timestamp)
+	INDEX(thread_id, timestamp)
+);
+CREATE TABLE record_removed(
+	thread_id INT UNSIGNED,
+	timestamp TIMESTAMP,
+	bin_id BINARY(16),
+	PRIMARY KEY(thread_id, timestamp, bin_id)
 );
 CREATE TABLE record_post(
 	-- name,mailのサイズは適当
-	thread_id INT UNSIGNED,
 	record_id INT UNSIGNED,
 	name CHAR(255) NOT NULL,
 	mail CHAR(255) NOT NULL,
 	body MEDIUMTEXT NOT NULL,
-	PRIMARY KEY(thread_id, record_id)
+	PRIMARY KEY(record_id)
 );
 CREATE TABLE record_attach(
 	-- suffixのサイズは適当
-	thread_id INT UNSIGNED,
 	record_id INT UNSIGNED,
 	attach MEDIUMBLOB NOT NULL,
 	suffix CHAR(8) NOT NULL,
-	PRIMARY KEY(thread_id, record_id)
+	PRIMARY KEY(record_id)
 );
-CREATE TABLE record_remove(
-	thread_id INT UNSIGNED,
+CREATE TABLE record_remove_notify(
 	record_id INT UNSIGNED,
 	remove_id BINARY(16) NOT NULL,
 	remove_stamp TIMESTAMP NOT NULL,
-	PRIMARY KEY(thread_id, record_id)
+	PRIMARY KEY(record_id)
 );
 CREATE TABLE record_signature(
 	-- 公開鍵の長さは86文字 : http://shingetsu.info/protocol/protocol-0.5-2.pdf
 	-- targetのサイズは適当
-	thread_id INT UNSIGNED,
 	record_id INT UNSIGNED,
 	pubkey CHAR(86) NOT NULL,
 	sign CHAR(64) NOT NULL,
 	target CHAR(64) NOT NULL,
-	PRIMARY KEY(thread_id, record_id)
+	PRIMARY KEY(record_id)
 );
 
 CREATE TABLE tagname(
@@ -83,21 +88,33 @@ CREATE TABLE node(
 
 DELIMITER $$
 
-CREATE TRIGGER insert_thread AFTER INSERT ON record FOR EACH ROW
+CREATE TRIGGER insert_record AFTER INSERT ON record FOR EACH ROW
 BEGIN
-	UPDATE thread SET records = record+1;
+	UPDATE thread SET thread.records = thread.records+1
+	WHERE thread.id = NEW.thread_id;
 	IF NEW.timestamp > (SELECT thread.timestamp FROM thread WHERE NEW.thread_id = thread.id) THEN
-		UPDATE thread SET timestamp = NEW.timestamp;
+		UPDATE thread SET timestamp = NEW.timestamp
+		WHERE thread.id = NEW.thread_id;
 	END IF;
 END;
 $$
-
-CREATE TRIGGER delete_thread AFTER DELETE ON record FOR EACH ROW
+CREATE TRIGGER delete_record AFTER DELETE ON record FOR EACH ROW
 BEGIN
-	UPDATE thread SET records = record-1;
-	UPDATE thread SET timestamp = (SELECT MAX(record.timestamp)
-		FROM thread INNER JOIN record ON thread.id = record.thread_id
-		WHERE thread.id = OLD.thread_id);
+	UPDATE thread SET thread.records = thread.records-1
+	WHERE thread.id = OLD.thread_id;
+END;
+$$
+
+CREATE TRIGGER insert_record_removed AFTER INSERT ON record_removed FOR EACH ROW
+BEGIN
+	UPDATE thread SET thread.removed_records = thread.removed_records+1
+	WHERE thread.id = NEW.thread_id;
+END;
+$$
+CREATE TRIGGER delete_record_removed AFTER DELETE ON record_removed FOR EACH ROW
+BEGIN
+	UPDATE thread SET thread.removed_records = thread.removed_records-1
+	WHERE thread.id = OLD.thread_id;
 END;
 $$
 

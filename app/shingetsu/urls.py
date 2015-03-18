411000384807f38cc4a49aacd70962f31a98bcae
yuuki <yuuki0xff@gmail.com>
2015-03-18 16:51:55 +0900

    shingetsu/urls.py: URLのtimestampに関する正規表現を正確に表した

--- app/shingetsu/urls.py
+++ app/shingetsu/urls.py
@@ -3,11 +3,10 @@ from app.shingetsu.views import *
 
 _node = '(?P<node>.+)'
 _file = '(?P<file>[^/]+)'
-_time = '('+'|'.join([
-	'(?P<time>[0-9]+)(/(?P<id>.+))?',
-	'((?P<stime>[0-9]+)?-(?P<etime>[0-9]+)?)',
-	])+')'
 _id = '(?P<id>[0-9a-fA-F]{32})'
+_time = '(?P<time>[0-9]+)'
+_timeRange = '((?P<stime>[0-9]+)?-(?P<etime>[0-9]+)?)'
+_time_id = '({}(/{})?|{})'.format(_time, _id, _timeRange)
 
 urlpatterns = patterns('',
 	url(r'^ping$', ping.as_view()),
@@ -15,8 +14,8 @@ urlpatterns = patterns('',
 	url(r'^join/'+_node+'?$', join.as_view()),
 	url(r'^bye/'+_node+'?$',  bye.as_view()),
 	url(r'^have/'+_file+'$', have.as_view()),
-	url(r'^get/'+_file+'/'+_time+'$', get.as_view()),
-	url(r'^head/'+_file+'/'+_time+'$', head.as_view()),
+	url(r'^get/'+_file+'/'+_time_id+'$', get.as_view()),
+	url(r'^head/'+_file+'/'+_time_id+'$', head.as_view()),
 	url(r'^update/'+_file+'/'+_time+'/'+_id+'/('+_node+')?$', update.as_view()),
 	url(r'^recent/'+_time+'$', 'recent'),
 )

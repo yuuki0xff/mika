09520289965ebe6f9df8b5e6886f017c57fc1fb3
yuuki <yuuki0xff@gmail.com>
2015-03-22 18:52:59 +0900

    lib/utils: 時刻の変換関数を定義した

---
+++ lib/utils.py
@@ -0,0 +1,23 @@
+
+from datetime import datetime
+import time
+
+def timestamp2str(ts):
+	if ts == 0 or ts is None:
+		return '0000-00-00 00:00:00'
+	else:
+		return str(timestamp2datetime(ts))
+
+def str2timestamp(s):
+	if s is None:
+		return 0
+	return int(s)
+
+def datetime2timestamp(dt):
+	if dt is None:
+		return 0
+	return time.mktime(dt.timetuple())
+
+def timestamp2datetime(ts):
+	return datetime.fromtimestamp(ts)
+

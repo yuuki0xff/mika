f21d22f35ebb343c16c1a9638b4e8d51dc1122f7
yuuki <yuuki0xff@gmail.com>
2015-11-22 22:24:29 +0900

    clean up

--- docker/mika/startup.sh
+++ docker/mika/startup.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -euv
-
-REPOSITORY=${REPOSITORY:-https://github.com/yuuki0xff/mika}
 export MIKA_DB_ADDR=${MYSQL_PORT_3306_TCP#tcp://}
 
-#NGINX=${NGINX_PORT_80_TCP#tcp://}
-
-#git clone $REPOSITORY /srv
 cd /srv
-#./manage.py syncdb
-
-#mysql -uroot -proot <<EOS
-#create database mika if not exists;
-#EOS
-
 exec uwsgi --ini ./core/uwsgi.ini
 

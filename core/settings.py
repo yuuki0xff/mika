4f0e05866dbc9a94cf4db5bd01a118ddf6155db8
yuuki <yuuki0xff@gmail.com>
2015-11-28 05:08:11 +0900

    disable csrf

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
 #     'django.contrib.admin',
-    'django.contrib.auth',
+#     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -65,9 +65,9 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )

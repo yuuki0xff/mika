480e14b4d6e61c7574fc692655240f134a5ead61
yuuki <yuuki0xff@gmail.com>
2016-01-31 20:34:01 +0900

    clean up settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,53 +1,29 @@
 """
 Django settings for mika project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
-_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.extend([
-	_ROOT_DIR,
-	_ROOT_DIR+'/require'])
 
 APPLICATION_NAME='Mika'
 PROTOCOLS = ['shinGETsu/0.7', 'Mikaduki/0.1']
 VERSION = '0.3.0'
 
-if all((x in os.environ) for x in ['MIKA_DB_TYPE', 'MIKA_DB_AUTH', 'MIKA_DB_ADDR', 'MIKA_DB_NAME',]):
-	DB_ADDRESS = '{}://{}@{}/{}'.format(
-			os.environ['MIKA_DB_TYPE'],
-			os.environ['MIKA_DB_AUTH'],
-			os.environ['MIKA_DB_ADDR'],
-			os.environ['MIKA_DB_NAME'],
-			)
-else:
-	with open(_ROOT_DIR+'/core/db.address') as _fp:
-		DB_ADDRESS=_fp.read()
-
 MESSAGE_QUEUE_SOCK_FILE = "/run/mika/msgq.sock"
 MAX_THREADS = 16
 MAX_CONNECTIONS = 16
 HTTP_TIMEOUT = 16
-MAX_NODES = 50
+MAX_NODES = 10
 
 RECENT_INTERVAL=30*60
 PING_INTERVAL=5*60
-JOIN_INTERVAL=30*60
+JOIN_INTERVAL=5*60
 
+NODE_NAME = 'test'
 # NODE_NAME = '<IP_ADDRESS>:<PORT>+server_api'
 # example:
 #   '192.0.2.2:80+server_api'
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+assert(NODE_NAME)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$#7@64zk2+aqqmg3fsd3$9ucs4&!2#t-pvlwp4$j270h_x&x+*'
@@ -58,6 +34,13 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+
+# Set the module search path
+_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.extend([
+	_ROOT_DIR,
+	_ROOT_DIR+'/require'])
 
 
 # Application definition
@@ -88,6 +71,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# For SQLAlchemy
+if all((x in os.environ) for x in ['MIKA_DB_TYPE', 'MIKA_DB_AUTH', 'MIKA_DB_ADDR', 'MIKA_DB_NAME',]):
+	DB_ADDRESS = '{}://{}@{}/{}'.format(
+			os.environ['MIKA_DB_TYPE'],
+			os.environ['MIKA_DB_AUTH'],
+			os.environ['MIKA_DB_ADDR'],
+			os.environ['MIKA_DB_NAME'],
+			)
+else:
+	with open(_ROOT_DIR+'/core/db.address') as _fp:
+		DB_ADDRESS=_fp.read()
+
+# For Django
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,7 +103,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/

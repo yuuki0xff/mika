d52fa59ec4f841b37a523fd4be1e576f026f1cf5
yuuki <yuuki0xff@gmail.com>
2016-01-31 17:05:27 +0900

    docker/mika/startup.sh: fix bug
    exec suをすると何故かコンテナが落ちてしまう

--- docker/mika/startup.sh
+++ docker/mika/startup.sh
@@ -11,5 +11,5 @@ mkdir -pm 770 /run/mika
 chown mika:mika /run/mika
 
 su -mc '/srv/manage.py msgqueue_daemon' mika &
-exec su -mc 'uwsgi --ini /srv/core/uwsgi.ini' mika
+su -mc 'uwsgi --ini /srv/core/uwsgi.ini' mika
 
